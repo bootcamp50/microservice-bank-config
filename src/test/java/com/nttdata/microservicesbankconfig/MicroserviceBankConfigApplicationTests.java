0e8e28ecfa1670429becebe133ab800f84a07bda
Cesar Sanchez <csanmont@emeal.nttdata.com>
2024-03-19 14:26:31 -0500

    initial version

---
+++ src/test/java/com/nttdata/microservicesbankconfig/MicroserviceBankConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.microservicesbankconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBankConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
